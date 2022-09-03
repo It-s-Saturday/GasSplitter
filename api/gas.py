e1b33b7fe18e3544e16d66f4c5a87eca1168f8a7
Mostafa Damir <mad237@njit.edu>
2022-09-03 10:09:42 -0400

    added Gas api Key

--- api/gas.py
+++ api/gas.py
@@ -1,24 +1,25 @@
 from asyncio.windows_events import NULL
 from genericpath import isfile
+from car_secret import GAS_API_KEY
 import json
 import requests
 import os
 
 cwd = os.getcwd()
-print(isfile(f'{cwd}/api/mock_gas_price.json'))
+print(isfile(f"{cwd}/api/mock_gas_price.json"))
 
-if  isfile(f'{cwd}/api/mock_gas_price.json'):
-    with open(f'{cwd}/api/mock_gas_price.json') as f:
+if isfile(f"{cwd}/api/mock_gas_price.json"):
+    with open(f"{cwd}/api/mock_gas_price.json") as f:
         mock_gas_price = json.load(f)
-        print(mock_gas_price['result']['state']['gasoline'])
+        print(mock_gas_price["result"]["state"]["gasoline"])
 else:
     url = "https://gas-price.p.rapidapi.com/stateUsaPrice"
 
-    querystring = {"state":"WA"}
+    querystring = {"state": "WA"}
 
     headers = {
-        "X-RapidAPI-Key": "7ecb8b990fmsh4dd92222a29eb72p1de777jsn388d89d19302",
-        "X-RapidAPI-Host": "gas-price.p.rapidapi.com"
+        "X-RapidAPI-Key": GAS_API_KEY,
+        "X-RapidAPI-Host": "gas-price.p.rapidapi.com",
     }
 
     response = requests.request("GET", url, headers=headers, params=querystring)
